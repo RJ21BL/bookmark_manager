31fa33b68ae58a81e8ea1cfae2a2891838334b89
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-04 14:05:35 +0000

    Conditional statement in Bookmark class to connect to specific DB when required. Adjusted spec to compliment.

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,10 +4,20 @@ require 'bookmark'
 
 describe Bookmark do
   describe '.all' do
-    it 'returns all bookmarks' do
+    it 'returns a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.adobe.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.tableplus.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.ufc.com');")
+
       bookmarks = Bookmark.all
+
       expect(bookmarks).to include('http://www.adobe.com')
       expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.tableplus.com')
+      expect(bookmarks).to include('http://www.ufc.com')
     end
   end
 end
